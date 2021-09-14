e48a73a17bc732ff8dddfc95a8324dfbcdbc71f4
aphkyle <81857274+aphkyle@users.noreply.github.com>
2021-09-14 17:10:54 +0800

    Update pre-main.py

--- pre-main.py
+++ pre-main.py
@@ -2,6 +2,8 @@ import threading
 import time
 import io
 
+import cv2
+import numpy as np
 import pytesseract
 from PIL import Image
 from ppadb.client import Client as AdbClient
@@ -11,6 +13,9 @@ the coordinates in your device might be different
 be sure to modify the variable `number`
 the following position is only for insane mode
 """
+
+GREY_HIGH = np.array([20, 20, 20])
+GREY_LOW = np.array([190, 190, 190])
 
 numbers = {
     2: (100, 1095),
@@ -32,15 +37,25 @@ numbers = {
 }
 
 def _ocr(device):
+    b = np.array(np.frombuffer(io.BytesIO(device.screencap())).crop(
+                    (165, 370, 665, 900)
+            ).convert("RGB"))
+     
+    hsv = cv2.cvtColor(b, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, GREY_LOW, GREY_HIGH)
+    b[mask > 0] = (17, 17, 19)
+
+
     result = ''.join(filter(str.isdigit, pytesseract.image_to_string(
-                Image.open(io.BytesIO(device.screencap())).crop(
-                    (165, 370, 665, 730)
-                ),
+                Image.fromarray(b),
                 lang="eng",
                 config="--oem 3 --psm 6 -c tessedit_char_whitelist=0123456789",
             )
         )
     )
+
+    cv2.imwrite('testing.png', b) # this line is for testing, you can remove if you wanted to
+
     return int(result) if result != "" else _ocr(device)
 
 def prime_tap(device, n):
@@ -50,7 +65,7 @@ def prime_tap(device, n):
         while (n % d) == 0:
             if d in numbers:
                 n2 = numbers[d]
-                t = threading.Thread(target=device.shell, args=(f"input tap {n2[0]} {n2[1]}",)) # number buttons
+                t = threading.Thread(target=device.shell, args=(f"input tap {n2[0]} {n2[1]}",))
                 threads.append(t)
                 t.start()
             else:
@@ -60,14 +75,14 @@ def prime_tap(device, n):
     if n > 1:
         if n in numbers:
             n2 = numbers[n]
-            t = threading.Thread(target=device.shell, args=(f"input tap {n2[0]} {n2[1]}",)) # number buttons
+            t = threading.Thread(target=device.shell, args=(f"input tap {n2[0]} {n2[1]}",))
             threads.append(t)
             t.start()
         else:
             return
     for thread in threads:
         thread.join()
-    device.shell("input tap 600 1335",) # attack button
+    device.shell("input tap 600 1335",)
 
 def main():
     client = AdbClient()
@@ -75,14 +90,14 @@ def main():
     device = client.devices()
     device = device[0]
 
-    input()
+    input() # press enter
 
     while True:
         try:
             result = _ocr(device)
             print(result)
             prime_tap(device, result)
-            time.sleep(1.7)
+            time.sleep(2.6)
         except KeyboardInterrupt:
             break
         except Exception as e:
